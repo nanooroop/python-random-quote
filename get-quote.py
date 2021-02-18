d91c8d35d5a6b573c50fdd9f6310dabeac2f81df
SJCY-18 <sjcy-18@CYS18-KALI01.cyber.int>
2021-02-18 01:15:00 -0400

    Exploring python

--- get-quote.py
+++ get-quote.py
@@ -10,7 +10,9 @@ def primary():
   last = 13
   rnd = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd]).replace('\n', '')
+  rnd = random.randint(0,last)
+  print(quotes[rnd]).replace('\n', '')
 
 if __name__== "__main__":
   primary()
